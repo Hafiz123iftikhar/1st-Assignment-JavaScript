5c1869ddf01810f6222a7938709172468c5de5e0
Hafiz123iftikhar <155850590+Hafiz123iftikhar@users.noreply.github.com>
2024-03-09 10:13:19 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let firstname = prompt(" Please Enter Your First Name Here");
+let lastname = prompt("Please Enter Your Last Name Here");
+
+console.log(firstname+" "+lastname);
+
+alert("Hello, "+ firstname +" "+ lastname+". Wellcome To MyWebsite!" );
