99981e5c012dc31d6d8a3015da2bdbd503782481
Hafiz123iftikhar <155850590+Hafiz123iftikhar@users.noreply.github.com>
2024-03-10 04:20:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let firstname = prompt(" Please Enter Your First Name Here");
+
+
+let firstname = prompt("Please Enter Your First Name Here");
+let middlename = prompt("Please Enter Your Middle Name Here");
 let lastname = prompt("Please Enter Your Last Name Here");
 
-console.log(firstname+" "+lastname);
+console.log(firstname+" "+middlename+" "+lastname);
 
-alert("Hello, "+ firstname +" "+ lastname+". Wellcome To MyWebsite!" );
+alert("Hello, "+firstname +" "+middlename+" "+lastname+". Welcome to my website !");
